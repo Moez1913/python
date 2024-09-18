5d56598aca75510563dfb33b761bc38d6bbcdbb6
Moez1913 <kruhankhan1913@gmail.com>
2024-09-18 13:11:36 +0600

    this is my fist commit

---
+++ Beginner practice set/hi.py
@@ -0,0 +1,50 @@
+#add,subtrac,multiply,division,exponant,Modulus two number 
+'''a=20
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)'''
+
+#simple interest calculation:
+'''
+principal=1000
+rate=5
+time=2
+interest=(principal*rate*time)/100
+print(interest)
+'''
+# are of circle
+'''
+import math
+redius=5
+area=math.pi*(redius*2)
+print(area)
+'''
+
+#convert celcius to fahrenheit
+'''
+cl=25
+fh=(cl*(9/5))+32
+print(fh)
+'''
+
+# chake even or odd
+'''
+n=int(input("enter your number"))
+if n%2==0:
+    print("even")
+else:
+    #print("odd")
+    '''
+
+# find the largest number 
+"""
+a=int(input("Enter the first number:"))   
+b=int(input("Enter the 2nd number:"))   
+c=int(input("Enter the 3rd number:"))   
+largest=max(a,b,c)
+print(largest)"""
+
---
+++ basic.py
@@ -0,0 +1,12 @@
+
+fruits=["apple","lichhchi","banana"]
+for fruit in fruits:
+    print(fruit)
+
+for i in range(10):
+    print(i)
+
+#dictionary
+student_grads={"Sajib":85,"moez":95}
+for student,grad in student_grads.items():
+    print(f"{student} has a grad of {grad} ")
