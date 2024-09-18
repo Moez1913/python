d94fd4c976fc734649e92fc3d7c124c484dc2e07
Moez1913 <kruhankhan1913@gmail.com>
2024-09-18 13:33:02 +0600

    2nd commit

--- Beginner practice set/hi.py
+++ Beginner practice set/hi.py
@@ -48,3 +48,18 @@ c=int(input("Enter the 3rd number:"))
 largest=max(a,b,c)
 print(largest)"""
 
+#check prime number
+
+num=int(input("Enter the number"))
+if num>1:
+    for i in range(2,num):
+        if(num%i)==0:
+            print("Not prime")
+            break
+        else:
+            print("prime")
+         
+else:
+    print("Not prime")            
+
+
